29205d5dc147dc9c6a29dbd1bf3c3271ee95216a
Michael Zappa <me@michzappa.com>
2023-10-29 21:46:16 -0400

    Update beeper.

--- packages/beeper.nix
+++ packages/beeper.nix
@@ -4,6 +4,6 @@ appimageTools.wrapType1 {
   name = "beeper";
   src = fetchurl {
     url = "https://download.beeper.com/linux/appImage/x64";
-    hash = "sha256-Od8nuKeoQebpStR+33yJKMWf71Q9gvBqH10sGdd1PR4=";
+    hash = "sha256-ZuwPLYcVcjCCLdWSarY0oq0GUDiOrvNBgK/7ETb8OLg=";
   };
 }
